df7bcf221a056b4dcecda201bdd0ae767dbdfd80
KaranamK <kaushik.karanam@l-p-a.com>
2025-09-07 20:53:35 +0200

    adding app-5

---
+++ angular-app-5/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://localhost:5000/' //'http://api-service.app-5-dev.svc.cluster.local/api/' //'http://api.138.2.131.203.nip.io/api'
+}
