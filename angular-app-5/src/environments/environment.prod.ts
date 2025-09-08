83bbdad114cc4dad580fb14987697bad8d2e3a69
KaranamK <kaushik.karanam@l-p-a.com>
2025-09-08 14:33:16 +0200

    changing path in environment.prod.ts, ci-cd.yaml

--- angular-app-5/src/environments/environment.prod.ts
+++ angular-app-5/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     //production: true,
-    apiUrl: '/api' //'http://api-service.app-5-dev.svc.cluster.local/api/' //'http://api.138.2.131.203.nip.io/api'
+    apiUrl: '/' //'http://api-service.app-5-dev.svc.cluster.local/api/' //'http://api.138.2.131.203.nip.io/api'
 }
