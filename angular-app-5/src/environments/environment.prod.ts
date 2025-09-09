b572b9087511b3b9cb4e949e5c5f7d6983836bb7
KaranamK <kaushik.karanam@l-p-a.com>
2025-09-09 07:32:43 +0200

    change backend path in environment.prod.ts

--- angular-app-5/src/environments/environment.prod.ts
+++ angular-app-5/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
      production: true,
-     apiUrl: '/api/'
+     apiUrl: 'http://localhost:30082/api/'
+     //apiUrl: '/api/'
      //apiUrl: '/' //'http://api-service.app-5-dev.svc.cluster.local/api/' //'http://api.138.2.131.203.nip.io/api'
 }
