a2f503e9415b10637efb936692568950c6f830fe
KaranamK <kaushik.karanam@l-p-a.com>
2025-10-20 15:30:14 +0200

    Adjusting env variables prod

--- angular-app-5/src/environments/environment.prod.ts
+++ angular-app-5/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
      production: true,
      //apiUrl: 'http://localhost:30082/api/'
      //apiUrl: '/api/'
-     apiUrl: 'http://api-service.app-5-dev.svc.cluster.local/api/' //'http://api.138.2.131.203.nip.io/api'//
+     apiUrl: 'http://api.138.2.131.203.nip.io/api'// 'http://api-service.app-5-dev.svc.cluster.local/api/' //
 }
